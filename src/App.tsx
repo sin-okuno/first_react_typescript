b482c8fccfdf1628fe2f9999d7708b13f5fd7276
sin-okuno <73112448+sin-okuno@users.noreply.github.com>
2021-07-04 07:59:22 +0900

    get_display_data get_display_data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
-import { Practice1 } from "./practice";
 
 export default function App() {
+  const [fetchDatas, setFetchDatas] = useState<
+    Array<{ userId: number; id: number; title: string; completed: boolean }>
+  >();
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res.data);
+      setFetchDatas(res.data);
+    });
+  };
   return (
     <div className="App">
-      <h1>Hello TypeScript</h1>
-      <Practice1 />
+      <button onClick={onClickFetchData}>データ取得</button>
+      <table>
+        <thead>
+          <tr>
+            <th>userId</th>
+            <th>id</th>
+            <th>title</th>
+            <th>completed</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fetchDatas?.map((fetchData) => {
+            return (
+              <tr key={fetchData.id}>
+                <td>{fetchData.id}</td>
+                <td>{fetchData.userId}</td>
+                <td>{fetchData.title}</td>
+                <td>{fetchData.completed}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
